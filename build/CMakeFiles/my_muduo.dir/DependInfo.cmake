3b2c4da4e85b549354146594fb007998af878137
chenyandong <chenyandong@bytedance.com>
2025-06-07 13:34:35 +0000

    mac_init

--- build/CMakeFiles/my_muduo.dir/DependInfo.cmake
+++ build/CMakeFiles/my_muduo.dir/DependInfo.cmake
@@ -8,16 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/cyd/my_muduo/Logger.cc" "CMakeFiles/my_muduo.dir/Logger.cc.o" "gcc" "CMakeFiles/my_muduo.dir/Logger.cc.o.d"
-  "/home/cyd/my_muduo/Timestamp.cc" "CMakeFiles/my_muduo.dir/Timestamp.cc.o" "gcc" "CMakeFiles/my_muduo.dir/Timestamp.cc.o.d"
+  "/root/my_muduo/Logger.cc" "CMakeFiles/my_muduo.dir/Logger.cc.o" "gcc" "CMakeFiles/my_muduo.dir/Logger.cc.o.d"
+  "/root/my_muduo/Timestamp.cc" "CMakeFiles/my_muduo.dir/Timestamp.cc.o" "gcc" "CMakeFiles/my_muduo.dir/Timestamp.cc.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
